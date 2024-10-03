87d27fdc25bbb34ec7b995997727806fc326949e
Pooran14 <poorani.b.2023.csbs@ritchennai.edu.in>
2024-10-03 04:22:06 -0500

    Add files via upload

---
+++ selection .py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+arr = [64, 25, 12, 22, 11]
+print("Original array:", arr)
+sorted_arr = selection_sort(arr)
+print("Sorted array:", sorted_arr)
+
+
+
