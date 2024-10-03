52f2f57ed67d20deef54ff6a8a9f23d8ad2fce80
Pooran14 <poorani.b.2023.csbs@ritchennai.edu.in>
2024-10-03 04:28:30 -0500

    Add files via upload

---
+++ insertion  sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Original array:", arr)
+sorted_arr = insertion_sort(arr)
+print("Sorted array:", sorted_arr)
+
+
